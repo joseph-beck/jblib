fe5ebe715f82b8105e219f1a2b73f0480b5c578b
Joseph Beck <96671050+joseph-beck@users.noreply.github.com>
2026-06-14 21:15:26 +0100

    fix: eslint conf

--- packages/eslint-next/src/index.ts
+++ packages/eslint-next/src/index.ts
@@ -1,5 +1,6 @@
 import { type Linter } from 'eslint'
 import { globalIgnores } from 'eslint/config'
+
 import { defaults } from './rules/defaults.js'
 
 const config: Linter.Config[] = [...defaults, globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])]
